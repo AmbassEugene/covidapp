4ea91aff5546f33b2e4f4a861886475a5d55cdb3
Ambass Eugene <eugene.ambass@hotmail.com>
2021-06-21 08:45:41 +0530

    feat/state: adds a screen for selecting state

--- src/config/route.js
+++ src/config/route.js
@@ -6,15 +6,18 @@ import {
 } from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {HomeScreen} from '../screens/homeScreen/HomeScreen';
+import {StateScreen} from '../screens/StateScreen/StateScreen';
 
 const Stack = createStackNavigator();
 
 export const Routes = ({colorScheme}) => {
   return (
     <NavigationContainer
-      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    // theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
+    >
       <Stack.Navigator headerMode={false}>
         <Stack.Screen name="home" component={HomeScreen} />
+        <Stack.Screen name="state" component={StateScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/screens/StateScreen/StateScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const StateScreen = () => {
+  return (
+    <View>
+      <Text>I am StateScreen</Text>
+    </View>
+  );
+};
+
+export {StateScreen};
